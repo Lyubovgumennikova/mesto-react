24348d8e0954205d60c9fdfdfff75c130121e6ad
Lubov Gumennikova <lyubovgumennikova@yandex.ru>
2022-01-25 01:39:36 +0300

    mesto10extr

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function PopupWithForm({ name, title, children, isOpen, onClose , buttonText}) {
+  
+  const handleEscClose = (evt) => {
+    if (evt.key === "Escape") {
+      onClose();
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener("keyup", handleEscClose, false);
+
+    return () => {
+      document.removeEventListener("keyup", handleEscClose, false);
+    };
+  }, );
+
+  const handleOverlayClose = (evt) => {
+    if (evt.target === evt.currentTarget && isOpen) {
+      onClose();
+    }
+  };
+
   return (
     <div
       className={`${
@@ -8,6 +29,7 @@ function PopupWithForm({ name, title, children, isOpen, onClose , buttonText}) {
           ? `popup popup_type_${name} popup_opened`
           : `popup popup_type_${name}`
       }`}
+      onMouseUp={handleOverlayClose}
     >
       <button
         className="popup__close popup__close_type_edit"
